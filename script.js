1c7f8ab45aedf08914ff71624239a4783b3879e0
Treecher <treechcer@gmail.com>
2025-04-05 20:56:09 +0200

    started working on reworking the whole lang

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function runScript(){
 
     var special = {}
 
-    special["$start"] = "======= started =======" + "\n";
-    special["$end"] = "======= finshed =======" + "\n" + "\n";
+    special["$start"] = '======= started =======' + "\n";
+    special["$end"] = '======= finshed =======' + '\n' + "\n";
 
     special["$debugStart"] = "\n" + "====== debug started ======" + "\n";
     special["$debugEnd"] = "====== debug finished ======" + "\n" + "\n";
@@ -16,7 +16,7 @@ function runScript(){
     var lineByLine = input.value.split("\n");
 
     const defaultFunctionRegEx = [
-        /print\((.+)\);/i,
+        /(print)\((.+)\);/i,
     ];
 
     const defaultVariableRegEx = [
@@ -33,188 +33,89 @@ function runScript(){
         /(\w+) = special\s*"(\w+)"/
     ]
 
-    var calls = [];
+    var orders = [];
     var variables = [];
 
-    for (let j = 0; j < defaultFunctionRegEx.length; j++){
-        calls[j] = searchRegEx(defaultFunctionRegEx[j], lineByLine);
-    }
-
-    var isError = false;
-    var index;
-
-    for (let i = 0; i < defaultVariableRegEx.length; i++){
-        variables[i] = searchRegExVariables(defaultVariableRegEx[i], lineByLine);
-        try {
-            if (variables[i].duplicate){
-                isError = true;
-                index = variables[i].line;  
+    for (let line = 0; line < lineByLine.length; line++){
+        for (func of defaultFunctionRegEx){
+            var match = lineByLine[line].match(func);
+            if (match){
+                orders.push({name : match[1], value : match[2], line : line+1, bonus : match[0]})
             }
         }
-        catch(myBad){
 
-        }
-    }
-
-    if (!isError){
-        var special = changeStartEndVars(variables);
-    }
-    output.textContent += special["$start"];
-
-    var stop = errChech(lineByLine, -1, NaN);
-
-    if (isError){
-        stop = true;
-        errChech(lineByLine, 5, `Line ${index} error 3 reasigning value incorectly\n`);
-    }
-
-    if (!stop){
-        printingOut(calls[0], variables);
-
-        if (debug){
-            output.textContent += special["$debugStart"];
-            debugVar(variables);
-            debuger(calls);
-            output.textContent += special["$debugEnd"];
-        }
-    }
-
-    scrollToBottom();
-
-    output.textContent += special["$end"];
-}
-
-function changeStartEndVars(variables){
-    var retrunVar = {}
-    retrunVar["$start"] = "======= started =======" + "\n";
-    retrunVar["$end"] = "======= finshed =======" + "\n" + "\n";
-    retrunVar["$debugStart"] = "\n" + "====== debug started ======" + "\n";
-    retrunVar["$debugEnd"] = "====== debug finished ======" + "\n" + "\n";
-    for (let varArr of variables){
-        for (let singleVar of varArr){
-            if (singleVar.dataType == "special"){
-                if (singleVar.name == "$start"){
-                    retrunVar["$start"] = singleVar.value + "\n";
-                }
-                if (singleVar.name == "$end"){
-                    retrunVar["$end"] = singleVar.value + "\n";
-                }
-                if (singleVar.name == "$debugStart"){
-                    retrunVar["$debugStart"] = singleVar.value + "\n";
-                }
-                if (singleVar.name == "$debugEnd"){
-                    retrunVar["$debugEnd"] = singleVar.value + "\n";
-                }
+        for (let variable of defaultVariableRegEx){
+            var match = lineByLine[line].match(variable);
+            if (match){
+                orders.push({name : match[2], value : match[3], line : line+1, bonus : match[0], dataType : match[1]});
+                variables.push(({name : match[2], value : match[3], line : line+1, bonus : match[0], dataType : match[3]}));
             }
         }
     }
 
-    return retrunVar;
-}
+    output.textContent += special["$start"]
 
-function searchRegEx(regEx, lineByLine){
-    var returnThing = [];
-    for (let i = 0; i < lineByLine.length; i++) {
-        if (lineByLine[i][0] == "#" || lineByLine[i][0] == "/"){
-            continue;
+
+    for (let order of orders){
+        if (order.name == "print"){
+            printOut(order.value, variables);
         }
-        let match = lineByLine[i].match(regEx);
-        if (match) {
-            returnThing.push({line : match[0], pos : i, inside : match[1]});
-        }
-    }
 
-    return returnThing;
-}
-
-function searchRegExVariables(regEx, lineByLine){
-    var returnThing = [];
-    for (let i = 0; i < lineByLine.length; i++) {
-        if (lineByLine[i][0] == "#" || lineByLine[i][0] == "/"){
-            continue;
-        }
-        let match = lineByLine[i].match(regEx);
-        if (match) {
-
-            let tempObj = {name : match[2], pos : i, value : match[3], dataType : match[1]};
-            for (let j = 0; j < returnThing.length; j++) {
-                if (tempObj["name"] == returnThing[j]["name"]) {
-                    return {duplicate : true, line : i};
-                }
+        if (order.dataType == "int"){
+            if (debug){
+                debugVariables(order)
             }
-            returnThing.push({name : match[2], pos : i, value : match[3], dataType : match[1]});
         }
-    }
 
-    return returnThing;
-}
-
-function printingOut(printCall, variables){
-    for (p of printCall){
-        var out = fVarCheck(p, variables);
-        if (out != "err"){
-            output.textContent += out + "\n";
+        if (order.dataType == "text"){
+            if (debug){
+                debugVariables(order)
+            }
         }
-        else{
-            errChech(lineByLine, p.line+1, `Line ${p.pos + 1} error 2 (this variable doesn't exist, maybe you forgot to use ")` + "\n")
-        }
-    }
-}
 
-function fVarCheck(isThisVar, vars){
-    var strCheck = /"(\w+)"/
-    var numCheck = /(\d+)/
-    isThisVar = isThisVar.inside
-    var match = isThisVar.match(strCheck);
-    if (match){
-        return match[1];
-    }
-    var match = isThisVar.match(numCheck);
-    if (match){
-        return match[1];
-    }
-    for (let varArr of vars){
-        for(let varS of varArr){
-            if (isThisVar == varS.name){
-                return varS.value;
+        if (order.dataType == "bool"){
+            if (debug){
+                debugVariables(order)
+            }
+        }
+
+        if (order.dataType == "special"){
+            if (debug){
+                debugVariables(order)
             }
         }
     }
 
-    return "err";
+    output.textContent += special["$end"]
 }
 
-function debuger(calls){
-    
-}
+function printOut(print, vars){
+    var output = document.getElementById("output");
 
-function debugVar(variables){
-    for (let varArr of variables) {
-        for (let vars of varArr) {
-            output.textContent += `Line ${vars.pos + 1}: ${vars.name}, value: ${vars.value}, dataType: ${vars.dataType}\n`;
-        }
-    }
-}
+    var canWe = false;
 
-function scrollToBottom() {
-    let textarea = document.getElementById("output");
-    textarea.scrollTop = textarea.scrollHeight;
-}
-
-function errChech(lineByLine, lineIfFound, errMsg){
-    var err = false;
-    let out = document.getElementById("output");
-    if (lineIfFound != -1){
-        out.textContent += errMsg;
-    }
-    if (lineIfFound == -1){
-        for (let i = 0; i < lineByLine.length; i++){
-            if (lineByLine[i][lineByLine[i].length-1] != ";" && lineByLine[i].match(/^\S+/)){
-                out.textContent += `Line ${i + 1} error 1 (no semicolon)` + "\n"
-                err = true;
-            }
+    for (v of vars){
+        if (print == v){
+            canWe = true;
+            break;
         }
     }
 
-    return err;
+    if (!canWe && print[0] == '"' && print[print.length-1] == '"'){
+        print = print.slice(1,-1)
+        canWe = true;
+    }
+
+    if (canWe){
+        output.textContent += print + "\n";
+    }
+}
+
+function debugVariables(v){
+    printOut(`Line ${v.line}, ${v.name}, datatype ${v.value}`)
+}
+
+function clearConsole(){
+    var output = document.getElementById("output");
+    output.textContent = "";
 }
